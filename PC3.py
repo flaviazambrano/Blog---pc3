3792ff5d5e205aac2873a3d6a320c6420a5fc69e
flaviazambrano <a20240618@pucp.edu.pe>
2026-07-03 19:00:21 -0500

    Update PC3.py

--- PC3.py
+++ PC3.py
@@ -122,10 +122,17 @@ elif opciones == 'Experiencia':
     st.subheader("🎥 Video 1 - YouTube")
     # Inserta un video de YouTube directamente en la aplicación.
     # El usuario puede reproducirlo sin salir de Streamlit.
-    st.video("https://www.youtube.com/watch?v=X_Z7d04x9-E")
+    st.video("https://www.youtube.com/watch?v=SJ-UMN_M62k")
     # Agrega una breve descripción del video.
     st.caption(
-        "En este video se presenta ...., "
+        "En este video se presenta una explicación de los tipos de datos 
+        y las expresiones booleanas en Python, utilizando ejemplos prácticos 
+        desarrollados en Google Colab. A lo largo del video se explica el 
+        funcionamiento de los principales tipos de datos (string, integer, 
+        float y boolean), el uso de variables, la función print(), las 
+        f-strings, los operadores aritméticos y las expresiones booleanas, 
+        con el propósito de comprender cómo se utilizan estos conceptos 
+        básicos en la programación en Python. "
     )
 
     # Formato B
